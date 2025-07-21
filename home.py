4b3614e9eb719eb572f255c1a1a3685120286e73
takacs.alex <lxtak08@gmail.com>
2025-07-22 00:04:13 +0200

    WeatherApp1.1 fixed

--- home.py
+++ home.py
@@ -1,40 +1,47 @@
-#cd UC/WeatherApp
+#cd Users\Alex\UC\WeatherApp
 
 from flask import Flask, render_template, redirect, url_for, request
 import requests
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def home():
-    if request.method == 'POST': 
-        CITY = request.form['nm']
-        return redirect(url_for('city', cty=CITY))
-    else:
-        return render_template('home.html', homero='--C°', varos=CITY)
-        
-@app.route('/<cty>', methods=['GET', 'POST'])
-def city(cty):
+
+@app.get('/')
+def home_get():
+        return render_template('home.html', therm='--C°', place='')
+
+@app.post('/')
+def home_post():
+     CITY = request.form.get('nm')
+     return redirect(url_for('city_get', cty=CITY))
+    
+
+
+@app.get('/<cty>')
+def city_get(cty):
     URL = 'https://api.openweathermap.org/data/2.5/weather'
-    API_KEY = 'c5e9dd8bafe6a531746296d2fa76d36e'  
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
 
     params = {
         'q': cty,
-        'appid': API_KEY,
+        'appid': api_key,
         'units': 'metric',
         'lang': 'en'
     }        
 
     response = requests.get(URL, params=params).json()
-    # print(response)
     temp = round(response['main']['temp'], 1)
-    # print(temp)
     
-    if request.method == 'POST': 
-        CITY = request.form['nm']
-        return redirect(url_for('city', cty=CITY))
-    else:
-        return render_template('home.html', homero=temp, varos=cty)
+    return render_template('home.html', therm=f'{temp}C°', place=cty)
+
+@app.post('/<cty>')
+def city_post(cty):
+     CITY = request.form.get('nm')
+     return redirect(url_for('city_get', cty=CITY))
+     
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
